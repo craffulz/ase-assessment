0ad563c026c8753ba64b7fe23af8e8f58ede8bed
zuza <115052199+craffulz@users.noreply.github.com>
2025-07-25 15:54:30 +0200

    enabling vercel domain for our backend

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(e.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://ase-assessment.vercel.app/","http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     exposedHeaders: ["Authorization"],
